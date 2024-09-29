5761d0953f8095b170aba12ccf7c0a5af087602b
nclsndr <nclsndr1@gmail.com>
2024-09-29 21:07:03 +0200

    refactor(tokens-parser): parseRawInput

--- packages/w3c-design-tokens-parser/src/parseDesignTokens.ts
+++ packages/w3c-design-tokens-parser/src/parseDesignTokens.ts
@@ -1,7 +1,11 @@
 import { buildTreeState } from './state/buildTreeState.js';
 import { TokenTree } from './client/TokenTree.js';
 
-export function parseDesignTokens(value: unknown) {
-  const treeState = buildTreeState(value);
+/**
+ * Parse a JSON token tree into a TokenTree instance.
+ * @param jsonTokenTree
+ */
+export function parseDesignTokens(jsonTokenTree: unknown) {
+  const treeState = buildTreeState(jsonTokenTree);
   return new TokenTree(treeState);
 }
